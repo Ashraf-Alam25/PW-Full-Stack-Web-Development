461bd4252505f4ad504928345a2046174898f4c2
Ashraf Alam <ashrafalam.tech@gmail.com>
2026-08-27 05:42:24 +0530

    basic js syntax

---
+++ JavaScript/Arraysbasic.js
@@ -0,0 +1,7 @@
+let a = [1,3,4,4];
+console.log(a);
+let b= new Array("abs", "tas", "dajds");
+console.log(b);
+for(let i = 0; i < a.length; i++){
+    console.log(a[i]);
+}
---
+++ JavaScript/script.js
@@ -0,0 +1,48 @@
+// let age = 18;
+// if(age >= 18){
+//     console.log("you are allow to vote");
+// }
+// else{
+//     console.log("you are not allow to vote");
+// }
+
+// let signal = "gren";
+// if(signal == "red"){
+//     console.log("stop");
+// }
+// else if(signal == "yellow"){
+//     console.log("go slow");
+// }
+// else if(signal == "green"){
+//     console.log("go fast");
+    
+// }
+// else{
+//     console.log("invalid");
+    
+// }
+
+
+
+
+// let user = "admn";
+// switch(user){
+//     case "admin":
+//         console.log("he is admin");
+//         break;
+//     case "student":
+//         console.log("he is a student");
+//         break;
+//     default:
+//         console.log("i am defaulter");
+//         break;
+        
+// }
+
+
+
+
+
+
+isLoggedIn = false;
+isLoggedIn ? console.log("show home page") : console.log("please login ");
